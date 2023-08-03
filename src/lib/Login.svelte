055a6c5b34ca4f4028a769b69616ba1e6980faf1
Freddie Boys <freddieboys@hotmail.co.uk>
2023-08-03 13:35:37 +0100

    Moved login form to seperate file

---
+++ src/lib/Login.svelte
@@ -0,0 +1,96 @@
+<script>
+    export let showModal
+
+    let loggedIn = false
+
+    function handleClick(){
+        loggedIn = true
+    }
+</script>
+
+<div class="signin"> 
+    <div class="content"> 
+     <h2>LOGIN & PLAY</h2> 
+     <div class="form">
+        <p>Login with your prefered method</p>
+        <button on:click={{handleClick}} class="btn">Google</button>
+        <button on:click={{handleClick}} class="btn">Facebook</button>
+        <button on:click={{handleClick}} class="btn">Microsoft</button>
+      <button on:click={() => (showModal = true)} class="btn rules"> Rules </button>
+     </div> 
+    </div> 
+   </div>
+   
+<style>
+    @font-face {
+        font-family: 'Gelasio';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Gelasio Regular'), local('Gelasio-Regular'), url(https://fonts.gstatic.com/s/gelasio/v1/cIf9MaFfvUQxTTqS9C6hYQ.woff2) format('woff2');
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    }
+    .signin
+{
+  position: absolute;
+  width: 400px;
+  background: #222;  
+  z-index: 1000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 40px;
+  border-radius: 4px;
+  box-shadow: 0 15px 35px rgba(0,0,0,9);
+}
+.signin .content 
+{
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 5px;
+}
+.signin .content h2 
+{
+  font-size: 2em;
+  color: #0f0;
+  text-transform: uppercase;
+  margin-bottom: 5px;
+  font-family: 'Gelasio';
+}
+.signin .content .form 
+{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  text-align: center;
+}
+p {
+    color: #0f0;
+    font-weight: 400;
+    font-size: 20px;
+    font-family: 'Gelasio';
+}
+
+.btn {
+    background: #0f0;
+    color: #000;
+    font-family: 'Gelasio';
+    font-weight: 600;
+    font-size: 1.35em;
+    letter-spacing: 0.05em;
+    cursor: pointer;
+    padding: 10px;
+    border-radius: 4px;
+    border: none;
+    outline: none;
+    
+}
+
+.rules {
+    margin-top: 15px;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-	import Modal from './Modal.svelte';
+	import Modal from '$lib/Modal.svelte';
+    import Login from '$lib/Login.svelte';
 
 	let showModal = true;
 </script>
@@ -68,32 +69,8 @@
     <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> 
     <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> 
     <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> 
-   <div class="signin"> 
-    <div class="content"> 
-     <h2>Lets Play</h2> 
-     <div class="form"> 
-      <div class="inputBox"> 
-       <input type="text" required> <i>Username</i> 
-      </div> 
-      <div class="inputBox"> 
-        <input type="text" required> <i>First Name</i> 
-       </div>
-       <div class="inputBox"> 
-        <input type="text" required> <i>Last Name</i> 
-       </div>
-       <div class="inputBox"> 
-        <input type="email" required> <i>Email</i> 
-       </div>
-      <div class="inputBox"> 
-       <input type="password" required> <i>Password</i> 
-      </div>  
-      <div class="inputBox"> 
-       <input type="submit" value="Pay £1"> 
-      </div> 
-      <button on:click={() => (showModal = true)} class="btn"> Rules </button>
-     </div> 
-    </div> 
-   </div> 
+    
+    <Login bind:showModal />
   </section> <!-- partial --> 
  </body>
 
@@ -101,13 +78,6 @@
 
 
 <style>
-    @font-face {
-        font-family: 'Gelasio';
-        font-style: normal;
-        font-weight: 400;
-        src: local('Gelasio Regular'), local('Gelasio-Regular'), url(https://fonts.gstatic.com/s/gelasio/v1/cIf9MaFfvUQxTTqS9C6hYQ.woff2) format('woff2');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-    }
     .rules, .welcome {
         text-align: center;
     }
@@ -185,108 +155,6 @@ section span:hover
 {
   background: #0f0;
   transition: 0s;
-}
-
-section .signin
-{
-  position: absolute;
-  width: 400px;
-  background: #222;  
-  z-index: 1000;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 40px;
-  border-radius: 4px;
-  box-shadow: 0 15px 35px rgba(0,0,0,9);
-}
-section .signin .content 
-{
-  position: relative;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  gap: 40px;
-}
-section .signin .content h2 
-{
-  font-size: 2em;
-  color: #0f0;
-  text-transform: uppercase;
-}
-section .signin .content .form 
-{
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 25px;
-}
-section .signin .content .form .inputBox
-{
-  position: relative;
-  width: 100%;
-}
-section .signin .content .form .inputBox input 
-{
-  position: relative;
-  width: 100%;
-  background: #333;
-  border: none;
-  outline: none;
-  padding: 25px 10px 7.5px;
-  border-radius: 4px;
-  color: #fff;
-  font-weight: 500;
-  font-size: 1em;
-}
-section .signin .content .form .inputBox i 
-{
-  position: absolute;
-  left: 0;
-  padding: 15px 10px;
-  font-style: normal;
-  color: #aaa;
-  transition: 0.5s;
-  pointer-events: none;
-}
-.signin .content .form .inputBox input:focus ~ i,
-.signin .content .form .inputBox input:valid ~ i
-{
-  transform: translateY(-7.5px);
-  font-size: 0.8em;
-  color: #fff;
-}
-
-
-.signin .content .form .inputBox input[type="submit"]
-{
-  padding: 10px;
-  background: #0f0;
-  color: #000;
-  font-weight: 600;
-  font-size: 1.35em;
-  letter-spacing: 0.05em;
-  cursor: pointer;
-  font-family: Gelasio
-}
-input[type="submit"]:active
-{
-  opacity: 0.6;
-}
-.btn {
-    background: #0f0;
-    color: #000;
-    font-family: Gelasio;
-    font-weight: 600;
-    font-size: 1.35em;
-    letter-spacing: 0.05em;
-    cursor: pointer;
-    padding: 10px;
-    border-radius: 4px;
-    border: none;
-  outline: none;
 }
 @media (max-width: 900px)
 {
